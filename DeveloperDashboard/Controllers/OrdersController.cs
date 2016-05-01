8160014cc903cd926bf9ddeaac99c985f125aafa
Mike Nitchie <mikenitchie@gmail.com>
2016-05-01 17:16:23 -0400

    OrderID to ID

--- DeveloperDashboard/Controllers/OrdersController.cs
+++ DeveloperDashboard/Controllers/OrdersController.cs
@@ -18,7 +18,7 @@ namespace DeveloperDashboard.API.Controllers
 
 	public class Order
 	{
-		public int OrderID { get; set; }
+		public int ID { get; set; }
 		public string CustomerName { get; set; }
 		public string ShipperCity { get; set; }
 		public Boolean IsShipped { get; set; }
@@ -27,11 +27,11 @@ namespace DeveloperDashboard.API.Controllers
 		{
 			List<Order> OrderList = new List<Order>
 			{
-				new Order {OrderID = 10248, CustomerName = "Taiseer Joudeh", ShipperCity = "Amman", IsShipped = true },
-				new Order {OrderID = 10249, CustomerName = "Ahmad Hasan", ShipperCity = "Dubai", IsShipped = false},
-				new Order {OrderID = 10250,CustomerName = "Tamer Yaser", ShipperCity = "Jeddah", IsShipped = false },
-				new Order {OrderID = 10251,CustomerName = "Lina Majed", ShipperCity = "Abu Dhabi", IsShipped = false},
-				new Order {OrderID = 10252,CustomerName = "Yasmeen Rami", ShipperCity = "Kuwait", IsShipped = true}
+				new Order {ID = 10248, CustomerName = "Taiseer Joudeh", ShipperCity = "Amman", IsShipped = true },
+				new Order {ID = 10249, CustomerName = "Ahmad Hasan", ShipperCity = "Dubai", IsShipped = false},
+				new Order {ID = 10250,CustomerName = "Tamer Yaser", ShipperCity = "Jeddah", IsShipped = false },
+				new Order {ID = 10251,CustomerName = "Lina Majed", ShipperCity = "Abu Dhabi", IsShipped = false},
+				new Order {ID = 10252,CustomerName = "Yasmeen Rami", ShipperCity = "Kuwait", IsShipped = true}
 			};
 
 			return OrderList;
