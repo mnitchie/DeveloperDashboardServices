c06fbfc084c2d615256898972badff69a16c8710
Mike Nitchie <mikenitchie@gmail.com>
2016-04-30 20:22:24 -0400

    Implement the startup class

--- DeveloperDashboard/App_Start/WebApiConfig.cs
+++ DeveloperDashboard/App_Start/WebApiConfig.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
-namespace DeveloperDashboard
+namespace DeveloperDashboard.API
 {
     public static class WebApiConfig
     {
--- DeveloperDashboard/Startup.cs
+++ DeveloperDashboard/Startup.cs
@@ -1,11 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
 
-namespace DeveloperDashboard
+[assembly: OwinStartup(typeof(DeveloperDashboard.API.Startup))]
+namespace DeveloperDashboard.API
 {
 	public class Startup
 	{
+		public void Configuration( IAppBuilder app )
+		{
+			HttpConfiguration config = new HttpConfiguration();
+			WebApiConfig.Register( config );
+			app.UseWebApi( config );
+		}
 	}
 }
