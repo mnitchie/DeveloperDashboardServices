46e89bf6edb321083caa27f11981ae5abc65afcd
Mike Nitchie <mikenitchie@gmail.com>
2016-04-30 20:39:33 -0400

    Add user model and auth context

---
+++ DeveloperDashboard/Models/Auth/AuthContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace DeveloperDashboard.API.Models.Auth
+{
+	public class AuthContext : IdentityDbContext<IdentityUser>
+	{
+		public AuthContext() : base( "AuthContext" )
+		{
+
+		}
+	}
+}
