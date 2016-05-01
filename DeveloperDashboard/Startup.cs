1d64759e65c79ac51ef4f7016345f4687bfb73dd
Mike Nitchie <mikenitchie@gmail.com>
2016-04-30 21:42:31 -0400

    Add OAuth config to startup (currently broken. need to implement a class first...

--- DeveloperDashboard/Startup.cs
+++ DeveloperDashboard/Startup.cs
@@ -1,17 +1,33 @@
 ﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
+using System;
 using System.Web.Http;
 
-[assembly: OwinStartup(typeof(DeveloperDashboard.API.Startup))]
+[assembly: OwinStartup( typeof( DeveloperDashboard.API.Startup ) )]
 namespace DeveloperDashboard.API
 {
 	public class Startup
 	{
 		public void Configuration( IAppBuilder app )
 		{
+			ConfigureOAuth( app );
 			HttpConfiguration config = new HttpConfiguration();
 			WebApiConfig.Register( config );
 			app.UseWebApi( config );
+		}
+
+		private void ConfigureOAuth( IAppBuilder app )
+		{
+			OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions() {
+				AllowInsecureHttp = true,
+				TokenEndpointPath = new PathString( "/token" ),
+				AccessTokenExpireTimeSpan = TimeSpan.FromDays( 1 ),
+				Provider = new SimpleAuthorizationServerProvider()
+			};
+
+			app.UseOAuthAuthorizationServer( OAuthServerOptions );
+			app.UseOAuthBearerAuthentication( new OAuthBearerAuthenticationOptions() );
 		}
 	}
 }
