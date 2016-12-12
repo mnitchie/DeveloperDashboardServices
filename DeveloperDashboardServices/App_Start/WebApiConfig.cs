252e0c94837a7477dd2a7de270a6bc856b7c1a90
Mike Nitchie <nitchie@careevolution.com>
2016-12-11 20:44:55 -0500

    Delete Global.asax

--- DeveloperDashboardServices/App_Start/WebApiConfig.cs
+++ DeveloperDashboardServices/App_Start/WebApiConfig.cs
@@ -1,24 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace DeveloperDashboardServices
 {
-    public static class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
-            // Web API configuration and services
+	public static class WebApiConfig
+	{
+		public static void Register( HttpConfiguration config )
+		{
+			// Web API routes
+			config.MapHttpAttributeRoutes();
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
-    }
+			//var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+			//jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+		}
+	}
 }
