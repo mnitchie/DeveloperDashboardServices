48b15c15aa9a4fbf113f5716550e85a7b31995e8
Mike Nitchie <nitchie@careevolution.com>
2017-01-04 22:52:57 -0500

    Setup startup class

--- DeveloperDashboard/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DeveloperDashboard.WebApiApplication" Language="C#" %>
---
+++ DeveloperDashboard/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup( typeof( DeveloperDashboard.Startup ) )]
+namespace DeveloperDashboard
+{
+	public class Startup
+	{
+		public void Configuration( IAppBuilder app )
+		{
+			System.Diagnostics.Debug.WriteLine( "Hello" );
+		}
+
+	}
+}
